14801d45c07a395bcf1d84a9e2d36d814c46da0a
Vladyslav Protsenko <vladyslav.protsenko@ucu.edu.ua>
2022-05-25 22:50:15 +0300

    Added chat header

--- src/components/DMHeader/DMHeader.jsx
+++ src/components/DMHeader/DMHeader.jsx
@@ -5,10 +5,17 @@ import { Link } from "react-router-dom";
 const DMHeader = (props) => {
     const {goBackLink, imageLink, chatTitle} = props;
     return (
-        <Link to={goBackLink}>
-            <h1> {chatTitle} </h1>
-            <img src={imageLink}></img>
-        </Link>
+        <header>
+            <Link to={goBackLink}>
+                {/* <img src={arrow}></img> */}
+                <span className="return">Back to chats</span>
+            </Link>
+            <div className="user">
+                <span className="user-name"> {chatTitle} </span>
+                <span className="status">last seen recently</span>
+            </div>
+            <img className="avatar" src={imageLink}></img>
+        </header>
     );
 }
 
