742a7430914a00ad612e814526882ad8066bd4d6
alinamuliak <alina.muliak@ucu.edu.ua>
2022-06-03 16:10:17 +0300

    fixed read/delivered icons

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,14 +2,28 @@ import "./Message.css";
 
 const Message = (props) => {
     const {text, fromMe, time} = props;
-    return (
-        <>
-            <div className={`message ${fromMe ? 'out-message' : 'in-message'}`}>
-                <p className="text-content"> {text} </p>
-                <span className="message-time"> {time} </span>
-            </div>
-        </>
-    );
+    let read = true;
+    if (fromMe) {
+        return (
+            <>
+                <div className={`message out-message`}>
+                    <p className="message-text-container"> {text} </p>
+                    <span className="message-time"> {time} <img className={'message-tick-icon'}
+                                                                src={`/icons/${read ? 'read' : 'delivered'}.svg`}
+                                                                alt={'tick icon'}/> </span>
+                </div>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <div className={`message in-message`}>
+                    <p className="message-text-container"> {text} </p>
+                    <span className="message-time"> {time} </span>
+                </div>
+            </>
+        );
+    }
 }
 
 export default Message;
