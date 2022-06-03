8516c19892de77ddafbd294c00ddd196973a1b34
michael-2956 <work.michael.2956@gmail.com>
2022-06-04 01:05:38 +0300

    ccodestyle

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,34 +1,38 @@
 import "./Login.css";
 
 const LoginPage = () => {
+  return (
+    <>
+      <div class="logo">
+        <img className="logo_img" src="/logo512.png" alt="logo"></img>
+        <span className="sign_in">Sign in to Telegram</span>
+        <div className="subtitle">
+          <span className="info">
+            Please enter your phone number and password.
+          </span>
+        </div>
+        <form className="sign-in" method="post">
+          <div className="phone_num">
+            <input type="text" id="phone" required />
+            <label className="label-login">Phone number</label>
+          </div>
+          <div className="password">
+            <input type="password" id="password" required />
+            <label className="label-login">Password</label>
+          </div>
+          <button className="sign-button" type="button">
+            SIGN IN
+          </button>
+        </form>
+        <div className="subtitle2">
+          <span className="info">Don't have account yet?</span>
+        </div>
+        <button className="sign-up-button" type="button">
+          SIGN UP
+        </button>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <div class="logo">
-                <img className="logo_img" src="/logo512.png" alt="logo"></img>
-                <span className="sign_in">Sign in to Telegram</span>
-                <div className="subtitle">
-                    <span className="info">Please enter your phone number
-                        and password.</span>
-                </div>
-                <form className="sign-in" method="post">
-                    <div className="phone_num">
-                        <input type="text" id="phone" required/>
-                        <label className="label-login">Phone number</label>
-                    </div>
-                    <div className="password">
-                        <input type="password" id="password" required/>
-                        <label className="label-login">Password</label>
-                    </div>
-                    <button className="sign-button" type="button">SIGN IN</button>
-                </form>
-                <div className="subtitle2">
-                    <span className="info">Don't have account yet?</span>
-                </div>
-                <button className="sign-up-button" type="button">SIGN UP</button>
-            </div>
-        </>
-    );
-}
-
-export default LoginPage
+export default LoginPage;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,33 +1,36 @@
 import "./Register.css";
 
 const Register = () => {
-    return (
-        <>
-            <div className="logo">
-                <img className="logo_img" src="/logo512.png" alt="logo"></img>
-                <span className="sign_up_text">Sign up in Telegram</span>
-                <div className="subtitle">
-                    <span className="info">Please enter your phone number
-                        and password.</span>
-                </div>
-                <form className="sign-up" method="post">
-                    <div className="phone_num">
-                        <input type="text" id="phone" required/>
-                        <label>Phone number</label>
-                    </div>
-                    <div className="password">
-                        <input type="password" id="password" required/>
-                        <label>Password</label>
-                    </div>
-                    <div className="password-check">
-                        <input type="password" id="password-check" required/>
-                        <label>Confirm Password</label>
-                    </div>
-                    <button className="sign-up-button" type="button">SIGN UP</button>
-                </form>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="logo">
+        <img className="logo_img" src="/logo512.png" alt="logo"></img>
+        <span className="sign_up_text">Sign up in Telegram</span>
+        <div className="subtitle">
+          <span className="info">
+            Please enter your phone number and password.
+          </span>
+        </div>
+        <form className="sign-up" method="post">
+          <div className="phone_num">
+            <input type="text" id="phone" required />
+            <label>Phone number</label>
+          </div>
+          <div className="password">
+            <input type="password" id="password" required />
+            <label>Password</label>
+          </div>
+          <div className="password-check">
+            <input type="password" id="password-check" required />
+            <label>Confirm Password</label>
+          </div>
+          <button className="sign-up-button" type="button">
+            SIGN UP
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default Register
+export default Register;
