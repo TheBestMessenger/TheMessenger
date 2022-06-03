e4d420bb80344eab9825c854d3f6ec87b85c97a2
michael-2956 <work.michael.2956@gmail.com>
2022-06-04 00:52:13 +0300

    finish-edit-logic

--- src/components/Message/ContextMenu.jsx
+++ src/components/Message/ContextMenu.jsx
@@ -1,39 +1,17 @@
 import "./ContextMenu.css";
-import { useContext } from "react";
-import { BACKEND_SERVER_ROOT } from "../../config";
-import UserContext from "../../contexts/UserContext";
 
 const ContextMenu = (props) => {
-  const { message_id, chat_id } = props;
-  const userContext = useContext(UserContext);
-
-  function editButton() {
-    fetch(BACKEND_SERVER_ROOT + chat_id + "/editMessage", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        authorId: userContext.authorId,
-        message_id: message_id,
-        new_msg: "edited",
-      }),
-    });
-  }
-
-  function deleteButton() {
-    fetch(BACKEND_SERVER_ROOT + chat_id + "/deleteMessage", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        authorId: userContext.authorId,
-        message_id: message_id,
-      }),
-    });
-  }
+  const { message_id, handleEdit, handleDelete } = props;
 
   return (
     <>
       <div className={"message-context-menu"}>
-        <button className={"message-context-menu-button"} onClick={editButton}>
+        <button
+          className={"message-context-menu-button"}
+          onClick={() => {
+            handleEdit(message_id);
+          }}
+        >
           <img
             className={"icon"}
             src={`/icons/edit-message.svg`}
@@ -44,7 +22,9 @@ const ContextMenu = (props) => {
 
         <button
           className={"message-context-menu-button"}
-          onClick={deleteButton}
+          onClick={() => {
+            handleDelete(message_id);
+          }}
         >
           <img
             className={"icon"}
