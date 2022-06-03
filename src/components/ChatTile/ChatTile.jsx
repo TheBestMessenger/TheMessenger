d302500725774d953e4f3005dea56e4682a0a354
Stepan Seppukku <stepan.sushko@ucu.edu.ua>
2022-06-03 14:56:29 +0300

    Add partly main page

--- src/components/ChatTile/ChatTile.jsx
+++ src/components/ChatTile/ChatTile.jsx
@@ -2,12 +2,28 @@ import "./ChatTile.css";
 
 import { Link } from "react-router-dom";
 
+
 const ChatTile = (props) => {
     const {dmLink, imageLink, chatTitle} = props;
     return (
         <Link to={dmLink}>
-            <h1> {chatTitle} </h1>
-            <img src={imageLink}></img>
+        <ul>
+            <li>
+                <div class="friend">
+                    <div class="img-name">
+                    <img class = "ava"src={imageLink}></img>
+                    <div>
+
+                        <h1> {chatTitle} </h1>
+                        <p> kkkkkgok hjfohjg iofhjioghj ofj ghjgject joing whire tgtgj odfmgoko </p>
+                        
+                    </div>
+                     
+                    </div>
+                    <div class="time"><h2 class="p1">10:50</h2></div>
+                </div>
+            </li>
+        </ul>
         </Link>
     );
 }
