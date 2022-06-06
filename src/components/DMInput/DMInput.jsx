0a8f5e5d80d080e4694570f52f84151bccffe396
alinamuliak <alina.muliak@ucu.edu.ua>
2022-06-06 18:57:30 +0300

    added send by enter

--- src/components/DMInput/DMInput.jsx
+++ src/components/DMInput/DMInput.jsx
@@ -1,37 +1,59 @@
 import './DMInput.css';
 
-import { useState } from 'react';
+import React, {useState} from 'react';
+import Picker from 'emoji-picker-react';
+
 
 const DMInput = (props) => {
-  const { handleMessage, sendText } = props;
-  const [messageText, setMessageText] = useState('');
+    const {handleMessage, sendText} = props;
+    const [messageText, setMessageText] = useState('');
+    const [chosenEmoji, setChosenEmoji] = useState(null);
 
-  const handleSubmit = () => {
-    handleMessage(messageText);
-    setMessageText('');
-  };
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+    };
 
-  return (
-    <div className='input'>
-      <form className='form'>
-        <div className='cmd'>
-          <textarea
-            className='texta'
-            value={messageText}
-            onChange={(e) => setMessageText(e.target.value)}
-          />
+    const handleSubmit = () => {
+        handleMessage(messageText);
+        setMessageText('');
+    };
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleSubmit();
+        }
+    }
+
+    let show = false;
+
+    return (
+        <div className='input'>
+            <div className={'picker'}>
+                {show ? <Picker onEmojiClick={onEmojiClick}/> : ''}
+            </div>
+            <form className='form' onKeyPress={handleEnter}>
+                <div className='cmd'>
+                    <button className={'emoji-picker-button'} onClick={() => show = !show}>
+                        <img className={'emoji-picker-icon'} src={'/icons/emoji-icon.svg'} alt={'emoji-picker icon'}/>
+                    </button>
+                    <textarea
+                        className='texta'
+                        value={messageText} //+ chosenEmoji.emoji
+                        onChange={(e) => setMessageText(e.target.value)}
+                    />
+                </div>
+                <button
+                    className='btn'
+                    type='submit'
+                    value='Submit'
+                    onClick={handleSubmit}
+                >
+                    {sendText}
+                </button>
+            </form>
         </div>
-        <button
-          className='btn'
-          type='submit'
-          value='Submit'
-          onClick={handleSubmit}
-        >
-          {sendText}
-        </button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default DMInput;
