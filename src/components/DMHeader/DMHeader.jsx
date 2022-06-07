212899731be6689cd92e5b4550a4ea5b73402e06
Stepan Seppukku <stepan.sushko@ucu.edu.ua>
2022-06-07 21:15:55 +0300

    userinfo

--- src/components/DMHeader/DMHeader.jsx
+++ src/components/DMHeader/DMHeader.jsx
@@ -1,20 +1,21 @@
-import './DMHeader.css';
+import "./DMHeader.css";
+import UserInfo from '../UserInfo/UserInfo'
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const DMHeader = (props) => {
   const { goBackLink, imageLink, chatTitle } = props;
   return (
-    <header>
+    <header className = "header">
       <Link className={'back-to-chats'} to={goBackLink}>
         <p className={'return-chats-text'}> Back to chats </p>
       </Link>
-      <div className='user'>
-        <span className='user-name'> {chatTitle} </span>
-        <span className='status'>last seen recently</span>
+      <div className="user">
+        <span className="user-name"> {chatTitle} </span>
+        <span className="status">last seen recently</span>
       </div>
-      <img className='avatar' src={imageLink} />
-    </header>
+      <UserInfo username = {props.chatTitle} nickname = "12378623" telephone = "38081239123" photo = {props.imageLink}  />
+     </header>
   );
 };
 
--- src/components/DMInput/DMInput.jsx
+++ src/components/DMInput/DMInput.jsx
@@ -1,37 +1,57 @@
 import './DMInput.css';
 
-import { useState } from 'react';
+import React, {useState} from 'react';
+import Picker from 'emoji-picker-react';
+
 
 const DMInput = (props) => {
-  const { handleMessage, sendText } = props;
-  const [messageText, setMessageText] = useState('');
+    const {handleMessage, sendText} = props;
+    const [messageText, setMessageText] = useState('');
+    const [chosenEmoji, setChosenEmoji] = useState(null);
 
-  const handleSubmit = () => {
-    handleMessage(messageText);
-    setMessageText('');
-  };
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+        setMessageText(messageText + emojiObject.emoji)
+    };
 
-  return (
-    <div className='input'>
-      <form className='form'>
-        <div className='cmd'>
-          <textarea
-            className='texta'
-            value={messageText}
-            onChange={(e) => setMessageText(e.target.value)}
-          />
-        </div>
-        <button
-          className='btn'
-          type='submit'
-          value='Submit'
-          onClick={handleSubmit}
-        >
-          {sendText}
-        </button>
-      </form>
-    </div>
-  );
+    const handleSubmit = () => {
+        handleMessage(messageText);
+        setMessageText('');
+    };
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            handleSubmit();
+        }
+    }
+
+
+    return (<div className='input'>
+            <div className={'picker'}>
+                <Picker onEmojiClick={onEmojiClick}/>
+            </div>
+            <form className='form' onKeyPress={handleEnter}>
+                <div className='cmd'>
+                    <button className={'emoji-picker-button'} onClick={() => console.log('click')}>
+                        <img className={'emoji-picker-icon'} src={'/icons/emoji-icon.svg'} alt={'emoji-picker icon'}/>
+                    </button>
+                    <textarea
+                        className='texta'
+                        value={messageText}
+                        onChange={(e) => setMessageText(e.target.value)}
+                    />
+                </div>
+                <button
+                    className='btn'
+                    type='submit'
+                    value='Submit'
+                    onClick={handleSubmit}
+                >
+                    {sendText}
+                </button>
+            </form>
+        </div>);
 };
 
 export default DMInput;
