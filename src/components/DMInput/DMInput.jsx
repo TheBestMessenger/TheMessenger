d4c792f1c3eeff728b1d1b19eb00920349bc8388
alinamuliak <alina.muliak@ucu.edu.ua>
2022-06-08 11:55:36 +0300

    formatting

--- src/components/DMInput/DMInput.jsx
+++ src/components/DMInput/DMInput.jsx
@@ -1,71 +1,86 @@
 import './DMInput.css';
 
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Picker from 'emoji-picker-react';
 
-
 const DMInput = (props) => {
-    const {handleMessage, sendText, editMode, messageToEdit} = props;
-    const [messageText, setMessageText] = useState('');
-    const [chosenEmoji, setChosenEmoji] = useState(null);
-    const [showEmoji, setShowEmoji] = useState(false);
+  const { handleMessage, sendText, editMode, messageToEdit } = props;
+  const [messageText, setMessageText] = useState('');
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+  const [showEmoji, setShowEmoji] = useState(false);
 
-    document.addEventListener('mousedown', (e) => {
-        if (showEmoji && (e.target.className === 'message-container' || e.target.className === 'texta'
-            || e.target.className === 'input')) {
-            setShowEmoji(!showEmoji);
-        }
-    });
-
-    const onEmojiClick = (event, emojiObject) => {
-        setChosenEmoji(emojiObject);
-        setMessageText(messageText + emojiObject.emoji)
-    };
-
-    const handleSubmit = () => {
-        handleMessage(messageText);
-        setMessageText('');
-    };
-
-    const handleEnter = (e) => {
-        if (e.key === 'Enter' && !e.shiftKey) {
-            e.preventDefault();
-            handleSubmit();
-        }
+  document.addEventListener('mousedown', (e) => {
+    if (
+      showEmoji &&
+      (e.target.className === 'message-container' ||
+        e.target.className === 'texta' ||
+        e.target.className === 'input')
+    ) {
+      setShowEmoji(!showEmoji);
     }
+  });
 
-    useEffect(() => {
-        if (editMode) {
-            setMessageText(messageToEdit.msg);
-        }
-    }, [editMode]);
+  const onEmojiClick = (event, emojiObject) => {
+    setChosenEmoji(emojiObject);
+    setMessageText(messageText + emojiObject.emoji);
+  };
 
-    return (
-        <div className='input'>
-            {showEmoji ? <div className={'picker'}>
-                <Picker onEmojiClick={onEmojiClick}/>
-            </div> : ''}
-            <form className='form' onKeyPress={handleEnter}>
-                <div className='cmd'>
-                    <button className={'emoji-picker-button'} onClick={() => setShowEmoji(!showEmoji)}>
-                        <img className={'emoji-picker-icon'} src={'/icons/emoji.png'} alt={'emoji-picker icon'}/>
-                    </button>
-                    <textarea
-                        className='texta'
-                        value={messageText}
-                        onChange={(e) => setMessageText(e.target.value)}
-                    />
-                </div>
-                <button
-                    className='btn'
-                    type='submit'
-                    value='Submit'
-                    onClick={handleSubmit}
-                >
-                    {sendText}
-                </button>
-            </form>
-        </div>);
+  const handleSubmit = () => {
+    handleMessage(messageText);
+    setMessageText('');
+  };
+
+  const handleEnter = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit();
+    }
+  };
+
+  useEffect(() => {
+    if (editMode) {
+      setMessageText(messageToEdit.msg);
+    }
+  }, [editMode]);
+
+  return (
+    <div className='input'>
+      {showEmoji ? (
+        <div className={'picker'}>
+          <Picker onEmojiClick={onEmojiClick} />
+        </div>
+      ) : (
+        ''
+      )}
+      <form className='form' onKeyPress={handleEnter}>
+        <div className='cmd'>
+          <button
+            className={'emoji-picker-button'}
+            onClick={() => setShowEmoji(!showEmoji)}
+          >
+            <img
+              className={'emoji-picker-icon'}
+              src={'/icons/emoji.png'}
+              alt={'emoji-picker icon'}
+            />
+          </button>
+          <textarea
+            className='texta'
+            value={messageText}
+            onChange={(e) => setMessageText(e.target.value)}
+          />
+        </div>
+        <button
+          className='btn'
+          type='submit'
+          value='Submit'
+          onClick={handleSubmit}
+        >
+          {sendText}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default DMInput;
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,43 +2,42 @@ import './Message.css';
 import Fade from 'react-reveal/Fade';
 
 const Message = (props) => {
-    const {text, fromMe, time, edited, message_id} = props;
-    let read = false;
-    if (fromMe) {
-        return (
-            <>
-                <Fade right duration={250}>
-                    <div className={'message out-message'} message_id={message_id}>
-                        <p className={'message message-text-container'}> {text} </p>
-                        <span className={'message message-time message-time-out'}>
-            {' '}
-                            {`${edited ? 'edited' : ''} ${time}`}{' '}
-                            <img
-                                className={'message-tick-icon'}
-                                src={`/icons/${read ? 'read' : 'delivered'}.svg`}
-                                alt={'tick icon'}
-                            />{' '}
-          </span>
-                    </div>
-                </Fade>
-
-            </>
-        );
-    } else {
-        return (
-            <>
-                <Fade left duration={250}>
-                    <div className={'message in-message'}>
-                        <p className={'message message-text-container'}> {text} </p>
-                        <span className={'message message-time message-time-in'}>
-            {' '}
-                            {`${edited ? 'edited' : ''} ${time}`}{' '}
-          </span>
-                    </div>
-                </Fade>
-            </>
-        );
-    }
+  const { text, fromMe, time, edited, message_id } = props;
+  let read = false;
+  if (fromMe) {
+    return (
+      <>
+        <Fade right duration={250}>
+          <div className={'message out-message'} message_id={message_id}>
+            <p className={'message message-text-container'}> {text} </p>
+            <span className={'message message-time message-time-out'}>
+              {' '}
+              {`${edited ? 'edited' : ''} ${time}`}{' '}
+              <img
+                className={'message-tick-icon'}
+                src={`/icons/${read ? 'read' : 'delivered'}.svg`}
+                alt={'tick icon'}
+              />{' '}
+            </span>
+          </div>
+        </Fade>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Fade left duration={250}>
+          <div className={'message in-message'}>
+            <p className={'message message-text-container'}> {text} </p>
+            <span className={'message message-time message-time-in'}>
+              {' '}
+              {`${edited ? 'edited' : ''} ${time}`}{' '}
+            </span>
+          </div>
+        </Fade>
+      </>
+    );
+  }
 };
 
 export default Message;
