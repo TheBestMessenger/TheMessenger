f2d5f346df40a69ce77f6ad05f21c1a780020a34
alinamuliak <alina.muliak@ucu.edu.ua>
2022-06-08 23:05:07 +0300

    added user profile editing

--- src/components/DMInput/DMInput.jsx
+++ src/components/DMInput/DMInput.jsx
@@ -1,57 +1,64 @@
 import './DMInput.css';
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Picker from 'emoji-picker-react';
 
-
 const DMInput = (props) => {
-    const {handleMessage, sendText} = props;
-    const [messageText, setMessageText] = useState('');
-    const [chosenEmoji, setChosenEmoji] = useState(null);
+  const { handleMessage, sendText } = props;
+  const [messageText, setMessageText] = useState('');
+  const [chosenEmoji, setChosenEmoji] = useState(null);
 
-    const onEmojiClick = (event, emojiObject) => {
-        setChosenEmoji(emojiObject);
-        setMessageText(messageText + emojiObject.emoji)
-    };
+  const onEmojiClick = (event, emojiObject) => {
+    setChosenEmoji(emojiObject);
+    setMessageText(messageText + emojiObject.emoji);
+  };
 
-    const handleSubmit = () => {
-        handleMessage(messageText);
-        setMessageText('');
-    };
+  const handleSubmit = () => {
+    handleMessage(messageText);
+    setMessageText('');
+  };
 
-    const handleEnter = (e) => {
-        if (e.key === 'Enter' && !e.shiftKey) {
-            e.preventDefault();
-            handleSubmit();
-        }
+  const handleEnter = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit();
     }
+  };
 
-
-    return (<div className='input'>
-            <div className={'picker'}>
-                <Picker onEmojiClick={onEmojiClick}/>
-            </div>
-            <form className='form' onKeyPress={handleEnter}>
-                <div className='cmd'>
-                    <button className={'emoji-picker-button'} onClick={() => console.log('click')}>
-                        <img className={'emoji-picker-icon'} src={'/icons/emoji-icon.svg'} alt={'emoji-picker icon'}/>
-                    </button>
-                    <textarea
-                        className='texta'
-                        value={messageText}
-                        onChange={(e) => setMessageText(e.target.value)}
-                    />
-                </div>
-                <button
-                    className='btn'
-                    type='submit'
-                    value='Submit'
-                    onClick={handleSubmit}
-                >
-                    {sendText}
-                </button>
-            </form>
-        </div>);
+  return (
+    <div className='input'>
+      <div className={'picker'}>
+        <Picker onEmojiClick={onEmojiClick} />
+      </div>
+      <form className='form' onKeyPress={handleEnter}>
+        <div className='cmd'>
+          <button
+            className={'emoji-picker-button'}
+            onClick={() => console.log('click')}
+          >
+            <img
+              className={'emoji-picker-icon'}
+              src={'/icons/emoji-icon.svg'}
+              alt={'emoji-picker icon'}
+            />
+          </button>
+          <textarea
+            className='texta'
+            value={messageText}
+            onChange={(e) => setMessageText(e.target.value)}
+          />
+        </div>
+        <button
+          className='btn'
+          type='submit'
+          value='Submit'
+          onClick={handleSubmit}
+        >
+          {sendText}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default DMInput;
--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -1,31 +1,40 @@
-import "./EditProfile.css"
-import UserInfo from "../UserInfo/UserInfo";
-const EditProfile = (props) =>{
-    const {edit,editText,close} = props;
-    return(
-        <div className = "editContent">
-            <h4 className='profileEditText'>{props.editText}</h4>
-            <textarea className='changeProfileText'></textarea>
-            <button type="submit" className='btn'
-            onClick={() => {
-                document.querySelector(props.close).style.display = 'none';
-                document.querySelector('.modal-content').style.background = "#212121";
-                document.querySelector('.modal-content').style.opacity = 1;
-            }}
-            >Submit</button>
-            <div
-                 className='close'
-                 onClick={() => {
-                     document.querySelector(props.close).style.display = 'none';
-                     document.querySelector('.modal-content').style.background = "#212121";
-                     document.querySelector('.modal-content').style.opacity = 1;
-                 }}
-             >
-                 {''}+{''}
-             </div>
-        </div>
-    )
+import './EditProfile.css';
+import UserInfo from '../UserInfo/UserInfo';
 
-}
+const EditProfile = (props) => {
+  const { edit, editText, close, setProperty } = props;
+  return (
+    <div className='editContent'>
+      <h4 className='profileEditText'>{props.editText}</h4>
+      <textarea
+        className='changeProfileText'
+        onChange={(e) => {
+          setProperty(e.target.value);
+        }}
+      />
+      <button
+        type='submit'
+        className='btn'
+        onClick={() => {
+          document.querySelector(props.close).style.display = 'none';
+          document.querySelector('.modal-content').style.background = '#212121';
+          document.querySelector('.modal-content').style.opacity = 1;
+        }}
+      >
+        Submit
+      </button>
+      <div
+        className='close'
+        onClick={() => {
+          document.querySelector(props.close).style.display = 'none';
+          document.querySelector('.modal-content').style.background = '#212121';
+          document.querySelector('.modal-content').style.opacity = 1;
+        }}
+      >
+        {''}+{''}
+      </div>
+    </div>
+  );
+};
 
-export default EditProfile
+export default EditProfile;
